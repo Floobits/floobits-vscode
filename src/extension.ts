903218a6a2ddd96c29a51912ce4bf264158dbcba
Geoff Greer <geoff@greer.fm>
2017-03-06 22:17:10 -0800

    hey we figured out how to bind to change events

--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,27 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "floobits" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "floobits" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World!');
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+  vscode.window.onDidChangeActiveTextEditor(function (event: vscode.TextEditor) {
+    console.log('hello', event);
+  });
+  vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+    console.log(event.document.uri.fsPath, event.document.fileName, event.contentChanges.map(e => e.text).join(','));
+  });
 }
 
 // this method is called when your extension is deactivated
